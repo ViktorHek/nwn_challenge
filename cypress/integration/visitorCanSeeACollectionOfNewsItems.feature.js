380af237b4122d2a8b623b09a31806f2b3cc369d
Sanne <sannelindholm@hotmail.com>
2020-12-16 14:38:05 +0100

    Creating test for visitor can see news and adding data-cy to articleCard component

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,5 +1,19 @@
-describe('', () => {
-  it('', () => {
-    cy.visit('/')
-  });
+describe("A visitor can,", () => {
+	describe("if there are news,", () => {
+		beforeEach(() => {
+			cy.server();
+			cy.route({
+				method: "GET",
+				url: "http://newsapi.org/v2/top-headlines",
+				response: "fixture:news_index.json",
+			});
+			cy.visit("/");
+		});
+
+		it("successfully see list of news", () => {
+			cy.get("[data-cy='news-index']").within(() => {
+				cy.contains("DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017");
+			});
+		});
+	});
 });
