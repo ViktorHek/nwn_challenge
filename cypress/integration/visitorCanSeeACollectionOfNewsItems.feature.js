29a1abc255d3e59c1067d006a61b548a7f3c40bb
Sanne <sannelindholm@hotmail.com>
2020-12-18 18:04:13 +0100

    Rewriting test with beforeEach block, problem with stub

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,12 +1,23 @@
-describe("A visitor can", () => {
-	it("successfully see list of news", () => {
-		cy.visit("/");
-		cy.get("[data-cy='news-index']").within(() => {
-			cy.get(".header").should("exist");
-			cy.get(".image").should("exist");
-			cy.get(".meta").should("exist");
-			cy.get(".description").should("exist");
-			cy.get(".extra").should("exist");
+describe("A visitor can,", () => {
+	describe("if there are news,", () => {
+		beforeEach(() => {
+			cy.server();
+			cy.route({
+				method: "GET",
+				url: "http://newsapi.org/v2/top-headlines",
+				response: "fixture:news_index.json",
+			});
+			cy.visit("/")
+		});
+
+		it("successfully see list of news", () => {
+			cy.get("[data-cy='news-index']").within(() => {
+				cy.get(".header").should("exist");
+				cy.get(".image").should("exist");
+				cy.get(".meta").should("exist");
+				cy.get(".description").should("exist");
+				cy.get(".extra").should("exist");
+			});
 		});
 	});
 });
