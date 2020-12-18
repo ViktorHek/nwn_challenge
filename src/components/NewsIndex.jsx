a88c36ed0f2f13f760414e829fbb1ea5841e7b36
Sanne <sannelindholm@hotmail.com>
2020-12-18 16:20:59 +0100

    Importing useDispatch to NewsIndex and adding a get request via axios from NewsService module, also importing module.

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import NewsCard from './NewsCard'
+import NewsService from "../modules/NewsService"
 
 
 const NewsIndex = () => {
   const dispatch = useDispatch();
   const articles = useSelector((state) => state.newsFeed);
+  const fetchNews = async () => {
+		let articles = await NewsService.index();
+		dispatch({ type: "SET_NEWS_FEED", payload: articles });
+	};
   return (
     <>
       <NewsCard />
