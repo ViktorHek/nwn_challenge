fadcc89bb57a8f93adad1773eefe8b442f6d4bae
Sanne <sannelindholm@hotmail.com>
2020-12-18 12:54:00 +0100

    Adding params to NewsCard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Header, Card } from "semantic-ui-react";
 
-const NewsCard = (props) => {
+const NewsCard = (articles) => {
 	return (
 		<Card
 			data-cy="news-index"
-			image="https://editorial.fxstreet.com/images/Markets/Currencies/Digital%20Currencies/Bitcoin/bitcoins-52602600_Large.jpg"
-			header="DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017"
-			meta={`By: ${"Lorenzo Stroe"} at ${"FXStreet"}`}
-			description="DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu"
-			extra={<Header as="h6">`Published at ${"2020-12-10T20:29:20Z"}`</Header>}
+			image={articles.urlToImage}
+			header={articles.header}
+			meta={`By: ${articles.author} at ${articles.name}`}
+			description={articles.description}
+			extra={<Header as="h6">`Published at ${articles.publishedAt}`</Header>}
 		/>
 	);
 };
