348e5d32f4b4f65bda2d2bae91bd9f71e9b20f90
Sanne <sannelindholm@hotmail.com>
2020-12-18 17:33:26 +0100

    Updating props in NewsCard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Header, Card } from "semantic-ui-react";
 
-const NewsCard = (articles) => {
+const NewsCard = ({article}) => {
 	return (
 		<Card
 			data-cy="news-index"
-			image={articles.urlToImage}
-			header={articles.header}
-			meta={`By: ${articles.author} at ${articles.name}`}
-			description={articles.description}
-			extra={<Header as="h6">`Published at ${articles.publishedAt}`</Header>}
+			image={article.urlToImage}
+			header={article.title}
+			meta={`By: ${article.author} at ${article.source.name}`}
+			description={article.description}
+			extra={<Header as="h6">{`Published at ${article.publishedAt}`}</Header>}
 		/>
 	);
 };
