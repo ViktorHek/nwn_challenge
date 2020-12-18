abc6d91ce7cf798b9525bd0359ce09d81963096a
Sanne <sannelindholm@hotmail.com>
2020-12-18 17:01:50 +0100

    Adding and importing useEffect to fetchnews, adding map funtion to articles and Card.group to display 5 items per row

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import NewsCard from './NewsCard'
-import NewsService from "../modules/NewsService"
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import NewsCard from "./NewsCard";
+import NewsService from "../modules/NewsService";
+import { Card } from "semantic-ui-react";
 
 const NewsIndex = () => {
   const dispatch = useDispatch();
@@ -10,12 +10,15 @@ const NewsIndex = () => {
   const fetchNews = async () => {
 		let articles = await NewsService.index();
 		dispatch({ type: "SET_NEWS_FEED", payload: articles });
-	};
-  return (
-    <>
-      <NewsCard />
-    </>
-  )
-}
+  };
+  
+  useEffect(() => {
+		fetchNews();
+	}, []);
+  let articlesDisplay = articles.map((article) => {
+		return <NewsCard article={article} />;
+	});
+	return <Card.Group itemsPerRow={5}>{articlesDisplay}</Card.Group>;
+};
 
 export default NewsIndex
--- src/modules/NewsService.js
+++ src/modules/NewsService.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 const NewsService = {
   async index() {
-    let result = await axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=0226cdc8350248edb433d7f76eb053bd");
+    let result = await axios.get("/top-headlines?country=us&apiKey=0226cdc8350248edb433d7f76eb053bd");
     return result.data.articles;
   },
 
   async search(query) {
-    let result = await axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=0226cdc8350248edb433d7f76eb053bd`);
+    let result = await axios.get(`/everything?q=${query}&apiKey=0226cdc8350248edb433d7f76eb053bd`);
     return result.data.articles;
   }
 }
