a40dbe52113d27e0dc97979332418245844372fc
viktor <viktorkarlsson125@gmail.com>
2020-12-19 22:11:39 +0100

    deletion beforeEach in cypress test

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,16 +1,6 @@
 describe("A visitor can,", () => {
-	describe("if there are news,", () => {
-		beforeEach(() => {
-			cy.server();
-			cy.route({
-				method: "GET",
-				url: "http://newsapi.org/v2/top-headlines",
-				response: "fixture:news_index.json",
-			});
-			cy.visit("/")
-		});
-
 		it("successfully see list of news", () => {
+			cy.visit("/")
 			cy.get("[data-cy='news-index']").within(() => {
 				cy.get(".header").should("exist");
 				cy.get(".image").should("exist");
@@ -19,5 +9,4 @@ describe("A visitor can,", () => {
 				cy.get(".extra").should("exist");
 			});
 		});
-	});
 });
