4fd386402e3fdf194824d6d3f772a551afeedba9
viktor <viktorkarlsson125@gmail.com>
2020-12-19 23:13:24 +0100

    adding action type in rootReducer. modifying newsSearch method, unsuccessfully atm

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import NewsService from '../modules/NewsService'
-import { Input } from 'semantic-ui-react'
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import NewsService from "../modules/NewsService";
+import { Input } from "semantic-ui-react";
 
 const NewsSearch = () => {
-  return (
-    <Input 
-      action='Search' 
-      placeholder='Search...' 
-      data-cy='search_input'
-    />
-  )
-}
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.specificNews);
+  const fetchNews = async () => {
+    let articles = await NewsService.search();
+    dispatch({ type: "SEARCH_NEWS", payload: articles });
+  };
 
-export default NewsSearch
+  useEffect(fetchNews, []);
+
+  return (
+    <>
+      <Input
+        action="Search"
+        placeholder="Search..."
+        data-cy="search_input"
+        onChange={(event) => setSearchValue(event)}
+        onClick={(event) => performeSearch(event)}
+      />
+      <div data-cy="search_result">here the search result will go</div>
+    </>
+  );
+};
+
+export default NewsSearch;
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,15 +1,21 @@
-import initialState from '../store/initialState'
+import initialState from "../store/initialState";
 const rootReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case "SET_NEWS_FEED":
-			return {
-				...state,
+  switch (action.type) {
+    case "SET_NEWS_FEED":
+      return {
+        ...state,
         newsFeed: action.payload,
-			};
+      };
 
-		default:
-			return state;
-	}
+    case "SEARCH_NEWS":
+      return {
+        ...state,
+        specificNews: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
